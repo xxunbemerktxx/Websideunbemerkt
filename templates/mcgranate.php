43573ffcf8d7965c640b34ac15dc6dbf9a54d6ce
Unbemerkt <keno.schulz@arcor.de>
2017-07-02 17:23:21 +0200

    Update mcgranate.php

--- templates/mcgranate.php
+++ templates/mcgranate.php
@@ -1,4 +1,4 @@
-
+<?php if($loggedin == "true") {  ?>
 <div id="fader" class="card">
   <div class="card-header">
     Minecraft Plugin "Granate"
@@ -65,3 +65,20 @@
 	 </a>
   </div>
 </div>
+<?php } else { ?>
+<div class="card">
+  <div class="card-header">
+    Bitte Einloggen!
+  </div>
+  <div class="card-block forums-list">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">
+        <img class="forum_img" hight="100px" weight="100px" src="/assets/img/forum_no_picture.png" />
+        Bitte logge dich ein um diese Seite zu Sehen!
+      </li>
+	  </a>
+    </ul>
+  </div>
+</div>
+
+<?php } ?>
