a5603c63f3be2a13a4d909d4767252c96c06f72b
Unbemerkt <keno.schulz@arcor.de>
2017-07-02 17:21:46 +0200

    Update mctxpacks.php

--- templates/mctxpacks.php
+++ templates/mctxpacks.php
@@ -1,4 +1,4 @@
-
+<?php if($loggedin == "true") {  ?>
 
 <div id="fader" class="card">
   <div class="card-header">
@@ -13,3 +13,20 @@
     </ul>
   </div>
 </div>
+<?php } else { ?>
+<div class="card">
+  <div class="card-header">
+    Bitte Einloggen!
+  </div>
+  <div class="card-block forums-list">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">
+        <img class="forum_img" hight="100px" weight="100px" src="/assets/img/forum_no_picture.png" />
+        Bitte logge dich ein um diese Seite zu Sehen!
+      </li>
+	  </a>
+    </ul>
+  </div>
+</div>
+
+<?php } ?>
