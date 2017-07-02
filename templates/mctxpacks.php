ba420a202f14d0b81e0f136e9b76aec129d999d1
Unbemerkt <keno.schulz@arcor.de>
2017-07-02 17:03:03 +0200

    Add files via upload

---
+++ templates/mctxpacks.php
@@ -0,0 +1,15 @@
+
+
+<div id="fader" class="card">
+  <div class="card-header">
+    Minecraft Plugins
+  </div>
+  <div class="card-block forums-list">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">
+        <img class="forum_img" src="/assets/img/mcgrass.png" />
+        <a href="?p=unbpeck">Unbemerkt Pack</a>
+      </li>
+    </ul>
+  </div>
+</div>
